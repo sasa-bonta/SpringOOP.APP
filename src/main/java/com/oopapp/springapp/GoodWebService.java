c2a9778ea08a7758eef4a614ab96599b98e66c1c
sasa-bonta <bonta2000@mail.ru>
2019-11-14 10:54:46 +0200

    First spring

--- src/main/java/com/oopapp/springapp/GoodWebService.java
+++ src/main/java/com/oopapp/springapp/GoodWebService.java
@@ -19,20 +19,11 @@ public class GoodWebService {
         return List.of("Jorika", "Zinaida", "Sirgi", "Slavik", "Andrusa", "Viorica", "Aliona", "Agafia");
     }
 
-//        List<String> childrenfi181 = new ArrayList<String>();
-//        childrenfi181.add("Jorica");
-//        childrenfi181.add("Zinaida");
-//        childrenfi181.add("Sirghi");
-//        childrenfi181.add("Slavik");
-//        childrenfi181.add("Andrusa");
-//        childrenfi181.add("Viorica");
-//        childrenfi181.add("Aliona");
-//        childrenfi181.add("Agafia");
-    
+    List<String> childrenfi181 = new ArrayList<String>();
+
     @PostMapping("/addname/{name}")
     public List<String> addname(@PathVariable("name") String name){
 
-        List<String> childrenfi181 = new ArrayList<String>();
         childrenfi181.add("Jorica");
         childrenfi181.add("Zinaida");
         childrenfi181.add("Sirghi");
@@ -43,26 +34,12 @@ public class GoodWebService {
         childrenfi181.add("Agafia");
         childrenfi181.add(name);
 
-        //System.out.println(name);
-        //childrenfi181.add(name);
-        //return (name);
-
         childrenfi181.forEach(System.out::println);
         return (childrenfi181);
     }
 
-//    @GetMapping ("/all")
-//        public List<String> getAll(){
-//        List<String> childrenfi181 = new ArrayList<>();
-//        childrenfi181.add("Jorica");
-//        childrenfi181.add("Zinaida");
-//        childrenfi181.add("Sirghi");
-//        childrenfi181.add("Slavik");
-//        childrenfi181.add("Andrusa");
-//        childrenfi181.add("Viorica");
-//        childrenfi181.add("Aliona");
-//        childrenfi181.add("Agafia");
-//        //childrenfi181.add(name);
-//        return childrenfi181;
-//    }
+    @GetMapping("/all")
+    public List<String> childrenfi181(){
+        return (childrenfi181);
+    }
 }
